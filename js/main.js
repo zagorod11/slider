a959f54ef8ba5ef19c35db6d8f29fe539c9fd6eb
zagorod11 <64003625+zagorod11@users.noreply.github.com>
2020-05-10 14:59:48 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+$('.slider').slick({
+	prevArrow:'<button class="slick-arrow slick-next"><img src="images/vpravo.svg" alt="vpravo">  </button>',
+  nextArrow:'<button class="slick-arrow slick-prev"><img src="images/vlevo.svg" alt="vlevo">  </button>'
+  })
+});
+
+
+  
